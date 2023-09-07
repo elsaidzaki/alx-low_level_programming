58b635aaaad5831bec02dd4f5df9d4e68d388a56
elsaidzaki <elsaidzaki53@gmail.com>
2023-09-07 14:27:45 -0700

    low level programing c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
-#!/bin/bash
-include <stdio.h> 
+#include <stdio.h> 
+/**
+ * main - entry point 
+ * a progarm the prints a line 
+ * return:0
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
