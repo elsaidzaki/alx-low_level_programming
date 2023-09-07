519e895bd38ceeddb82440716aeebc1fd355d800
elsaidzaki <elsaidzaki53@gmail.com>
2023-09-07 15:37:42 -0700

    low level programing c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,22 @@
 #include<stdio.h>
 /**
- * main - entry size
- *
- * reurn zero
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	printf("size of char:%d byte(s)\n", sizeof(char));
-	printf("size of an int:%d byte(s)\n", sizeof(int));
-	printf("size of a long int :%d byte(s)\n", sizeof(long int));
-	printf("size of a long int int :%d byte(s)\n", sizeof(long long int);
-	printf("size of a float:%d btye(s)\n", sizeof(float);
+	int b;
+	char a;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+	printf("Size of an int: %d byte(s)\n", sizeof(b));
+	printf("Size of a long int: %d byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+	printf("Size of a float: %d byte(s)\n", sizeof(e));
 	return (0);
-	}
+}
+
