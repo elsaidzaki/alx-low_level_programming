ce48a728afffe8a4b0b80692b16fcd9e98982cd1
elsaidzaki <elsaidzaki53@gmail.com>
2023-09-08 14:50:21 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints the alphapt
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+char ala = 'abcdefghijklmnopqrstuvwxyz';
+
+putchar(ala);
+return (0);
+}
