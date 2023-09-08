ce9eee6317777975bdc37f644194802ce79a9fcf
elsaidzaki <elsaidzaki53@gmail.com>
2023-09-08 16:11:59 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print single digit number
+ *
+ * Return: always 0 (success0)
+ */
+int main(void)
+{
+int i;
+
+while (i <= 90)
+{
+putchar("%d", i);
+i++;
+}
+putchar("\n");
+return (0);
+}
