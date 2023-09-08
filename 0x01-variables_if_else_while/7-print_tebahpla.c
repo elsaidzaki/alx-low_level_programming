966b723c212be1485ddd2556ce56aef690a5df92
elsaidzaki <elsaidzaki53@gmail.com>
2023-09-08 16:30:03 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -9,10 +9,10 @@ int main(void)
 char cv[26] = "zyxwvutsrqponmlkjihgfedcba";
 int i;
 
-for (i = 1; i <= 24; i++)
+for (i = 0; i < 24; i++)
 {
 putchar(cv[i]);
 }
-putchar("\n");
+putchar('\n');
 return (0);
 }
