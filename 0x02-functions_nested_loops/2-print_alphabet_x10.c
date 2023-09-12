3d0a097f7620b8d87af1be95d2cc6a3982fce98f
elsaidzaki <elsaidzaki53@gmail.com>
2023-09-12 04:46:00 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - ten
+ */
+void print_alphabet_x10(void)
+{
+int i;
+char a;
+
+for (i = 1; i <= 10; i++)
+{
+for (a = 'a'; a <= 'z'; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
+}
